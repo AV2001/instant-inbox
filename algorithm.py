ecfe236be663e817d2deefa05de96e471623f4dc
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-03-05 09:45:01 +0400

    vectorize email_text feature for both train and test

--- algorithm.py
+++ algorithm.py
@@ -12,4 +12,12 @@ X = data['email_text']
 y = data['class']
 
 # perform 80/20 split on the dataset for training and testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# vectorize
+vectorizer = TfidfVectorizer()
+
+# converts text to numbes for performing classification
+X_train_vector = vectorizer.fit_transform(X_train)
+X_test_vector = vectorizer.transform(X_test)
