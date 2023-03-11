0a45527cb0cf41ae2b613d2cd2c9272b859e6309
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-03-11 11:12:27 +0400

    add functionality for log in form modal window

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// Accssing body element
+const body = document.querySelector('body');
+
+// 'Log In' form DOM elements
+const logInBtn = document.getElementById('log-in-btn');
+const logInForm = document.getElementById('log-in-form');
+const logInEmail = document.getElementById('log-in-email');
+const logInPassword = document.getElementById('log-in-password');
+const logInSubmitBtn = document.getElementById('log-in-submit-btn');
+
+// Close button for each of the forms
+const closeBtn = document.querySelectorAll('.close-btn');
+
+// Create new div for the overlay effect
+const overlayContainer = document.createElement('div');
+
+// Opens the 'Log In' form modal window
+logInBtn.addEventListener('click', () => {
+    logInForm.style.display = 'block';
+    overlayContainer.classList.toggle('overlay');
+    body.appendChild(overlayContainer);
+});
+
+// This close button is form the login form.
+closeBtn[0].addEventListener('click', (event) => {
+    event.preventDefault();
+    logInForm.style.display = 'none';
+    overlayContainer.classList.toggle('overlay');
+});
+
+// Close the form wen clicked on the overlay itself
+overlayContainer.addEventListener('click', () => {
+    logInForm.style.display = 'none';
+    overlayContainer.classList.toggle('overlay');
+});
