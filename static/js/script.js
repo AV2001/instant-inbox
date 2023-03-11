2783463615a5de9b32c7dde1558e625f77144438
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-03-11 18:57:03 +0400

    add functionality for create account form modal window

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,22 @@ const logInEmail = document.getElementById('log-in-email');
 const logInPassword = document.getElementById('log-in-password');
 const logInSubmitBtn = document.getElementById('log-in-submit-btn');
 
+// 'Log In' form DOM elements
+const createAccountBtn = document.getElementById('create-account-btn');
+const createAccountForm = document.getElementById('create-account-form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const createAccountEmail = document.getElementById('create-account-email');
+const createAccountPassword = document.getElementById(
+    'create-account-password'
+);
+const createAccountConfirmPassword = document.getElementById(
+    'create-account-confirm-password'
+);
+const createAccountSubmitBtn = document.getElementById(
+    'create-account-submit-btn'
+);
+
 // Close button for each of the forms
 const closeBtn = document.querySelectorAll('.close-btn');
 
@@ -23,15 +39,30 @@ logInBtn.addEventListener('click', () => {
     body.appendChild(overlayContainer);
 });
 
-// This close button is form the login form.
+// Opens the 'Create Account' form modal window
+createAccountBtn.addEventListener('click', () => {
+    createAccountForm.style.display = 'block';
+    overlayContainer.classList.toggle('overlay');
+    body.appendChild(overlayContainer);
+});
+
+// This close button is form the login form
 closeBtn[0].addEventListener('click', (event) => {
     event.preventDefault();
     logInForm.style.display = 'none';
     overlayContainer.classList.toggle('overlay');
 });
 
-// Close the form wen clicked on the overlay itself
+// This close button is for the create account form
+closeBtn[1].addEventListener('click', (event) => {
+    event.preventDefault();
+    createAccountForm.style.display = 'none';
+    overlayContainer.classList.toggle('overlay');
+});
+
+// Close the form when clicked on the overlay itself
 overlayContainer.addEventListener('click', () => {
     logInForm.style.display = 'none';
+    createAccountForm.style.display = 'none';
     overlayContainer.classList.toggle('overlay');
 });
