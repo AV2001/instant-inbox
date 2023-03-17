e3140e7df8ad0e3cdb1b4f766478f8097a72750c
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-03-17 19:05:23 +0400

    remove login and create account routes

--- main.py
+++ main.py
@@ -16,18 +16,6 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route('/create-account/', methods=['POST'])
-def create_account():
-    # Insert data from create account form to the database
-    return User().create_account()
-
-
-@app.route('/login/', methods=['POST'])
-def login():
-    # Check whether credentials entered in log in form match with those in the database
-    return User().login()
-
-
 if __name__ == '__main__':
     # set debug to True when in development mode
     app.run(debug=True)
