4af32366c9b77c3b6748ed849abd025d04dd1ef0
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-03-18 19:54:59 +0400

    load environment variables and store them

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template, session, url_for
+from msal import ConfidentialClientApplication
+from uuid import uuid4
+import secrets
+import requests
+from dotenv.main import load_dotenv
+import os
+
+
 from models import User
+
+
+load_dotenv()
+
+APPLICATION_ID = os.environ['APPLICATION_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+print(APPLICATION_ID)
+print(CLIENT_SECRET)
+
 
 app = Flask(__name__)
 
