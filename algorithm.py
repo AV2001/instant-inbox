7ce166718b89452ef8042d34a3fb48be420d72aa
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-04-01 20:49:42 +0400

    save the model as pickle and write function to predict tag based on email

--- algorithm.py
+++ algorithm.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+import pickle
 
 data = pd.read_csv('./data/emails.csv', sep=',', encoding='cp1252')
 
@@ -33,3 +34,28 @@ y_predict = classifier.predict(X_test_vector)
 accuracy = accuracy_score(y_test, y_predict)
 precision = precision_score(y_test, y_predict, average='weighted')
 recall = recall_score(y_test, y_predict, average='weighted')
+
+# Save the trained model and vectorizer
+with open('model.pkl', 'wb') as f:
+    pickle.dump(classifier, f)
+
+with open('vectorizer.pkl', 'wb') as f:
+    pickle.dump(vectorizer, f)
+
+
+def predict_tag(email):
+    # Load the saved mode and vectorizer
+    with open('model.pkl', 'rb') as f:
+        loaded_classifier = pickle.load(f)
+
+    with open('vectorizer.pkl', 'rb') as f:
+        loaded_vectorizer = pickle.load(f)
+
+    # Vectorize the input email
+    email_vector = loaded_vectorizer.transform([email])
+
+    # Make the prediction
+    predicted_tag = loaded_classifier.predict(email_vector)
+
+    # Return the first (and only) prediction
+    return predicted_tag[0]
